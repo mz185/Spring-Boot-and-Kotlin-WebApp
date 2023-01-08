f89fbea728fe9e7ae47e089d850a738c07ac5419
mz185 <marinoszenonos@gmail.com>
2023-01-08 20:55:35 +0200

    Create HelloController & hello template

---
+++ src/main/kotlin/com/example/demo/HelloController.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class HelloController {
+    @GetMapping("hello")
+    fun hello(
+        @RequestParam(value = "name", defaultValue = "stranger") name: String,
+        model: Model
+    ): String {
+        val greeting = String.format("Hello %s!", name)
+        model["greeting"] = greeting
+        return String.format("Hello %s!", name)
+    }
+}

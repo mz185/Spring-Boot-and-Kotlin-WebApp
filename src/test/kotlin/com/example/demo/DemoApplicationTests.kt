24abae18cc66178c1998f6299def073893c336ea
mz185 <marinoszenonos@gmail.com>
2023-01-09 17:32:59 +0200

    Create DemoApplicationTests

--- src/test/kotlin/com/example/demo/DemoApplicationTests.kt
+++ src/test/kotlin/com/example/demo/DemoApplicationTests.kt
@@ -1,13 +1,27 @@
 package com.example.demo
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
 
-@SpringBootTest
-class DemoApplicationTests {
-
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests(@Autowired val restTemplate: TestRestTemplate) {
 	@Test
-	fun contextLoads() {
+	fun `Assert blog page title, content and status code`() {
+		restTemplate.getForEntity<String>("/").run {
+			assertThat(statusCode).isEqualTo(HttpStatus.OK)
+			assertThat(body).contains("Blog</h1>")
+		}
 	}
 
+	@Test
+	fun `Assert greeting page name`() {
+		restTemplate.getForEntity<String>("/hello?name=marino").run {
+			assertThat(body).contains("Hello marino!")
+		}
+	}
 }
